47e38ccb8dfead17eff36f465140f1bd716d38a6
denio-rus <denio@yandex.ru>
2018-08-01 03:38:59 +0700

    lesson3. final version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_212113) do
+ActiveRecord::Schema.define(version: 2018_07_31_203417) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_07_30_212113) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
