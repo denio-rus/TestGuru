bedb3e4268b03dfa466cbf96a4d37bde9b4ac0bc
denio-rus <denio@yandex.ru>
2018-08-01 21:13:28 +0000

    lesson4. seed added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+
+  def tests_by(level)
+    list = []
+    User.where(id: id).test_taken.each do |test_id|
+    list << Test.find_by('level = :level AND id = :test_id', :level, :test_id)
+    end
+    list.compact!
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([{username: 'Vasya'}, {username: 'Alex'}, {username: 'Frank'}]) 
+
+category = Category.create!(title: 'Programming')
+test = Test.create(title:'Ruby', level: 1, category_id: category.id)
+question = Question.create(body: 'about Ruby,correct A', test_id: test.id)
+Answer.create([{body: 'A. correct for Ruby question', correct: true, question_id: question.id},
+              {body: 'B. incorrect for Ruby question', question_id:question.id}])
+
+test = Test.create(title:'Python', level: 2, category_id: category.id)
+question = Question.create(body: 'about Python,correct A', test_id: test.id)
+Answer.create([{body: 'A. correct for Python question', correct: true, question_id: question.id},
+              {body: 'B. incorrect for Python question', question_id:question.id}])
+
+category = Category.create!(title: 'Math')
+test = Test.create(title:'Geometry', level: 2, category_id: category.id)
+question = Question.create(body: 'about Geometry,correct A', test_id: test.id)
+Answer.create([{body: 'A. correct for Geometry question', correct: true, question_id: question.id},
+              {body: 'B. incorrect for Geometry question', question_id:question.id}])
+
+category = Category.create!(title: 'Space')
+test = Test.create(title:'Moon', category_id: category.id)
+question = Question.create(body: 'about Moon,correct A', test_id: test.id)
+Answer.create([{body: 'A. correct for Moon question', correct: true, question_id: question.id},
+              {body: 'B. incorrect for Moon question', question_id:question.id}])
