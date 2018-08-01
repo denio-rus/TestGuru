011d0e27991c8339f689b6ed2eeea85fe0cd7574
denio-rus <denio@yandex.ru>
2018-08-01 21:49:26 +0000

    lesson4. add class Result

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ApplicationRecord
-
-
   def tests_by(level)
-    list = []
-    User.where(id: id).test_taken.each do |test_id|
-    list << Test.find_by('level = :level AND id = :test_id', :level, :test_id)
-    end
-    list.compact!
+    taken_test_ids = Result.where(user_id: id).select(:test_id)
+    Test.where('level = :level AND id: taken_tests_id', :level, :taken_test_ids)
   end
 end
