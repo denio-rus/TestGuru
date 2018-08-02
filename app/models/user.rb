ae5184dcbb016a503052063728f48fd20d6e5c95
denio-rus <denio@yandex.ru>
2018-08-02 09:18:08 +0000

    lesson4. db changed to postgresql

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  def tests_by(level)
-    taken_test_ids = Result.where(user_id: id).select(:test_id)
-    Test.where('level = :level AND id: taken_tests_id', :level, :taken_test_ids)
+  def tests_by_level(level)
+    Result.where(user_id: id)
   end
 end
