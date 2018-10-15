f51418aeb9a0d7661850fc8347d3d1d89541c966
denio-rus <denio@yandex.ru>
2018-10-15 11:54:34 +0000

    lesson17. Admin

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,6 +2,6 @@ class FeedbackMailer < ApplicationMailer
   def send_to_admin(feedback_message, current_user)
     @user = current_user
     @message = feedback_message
-    mail to: "admin@admin.comm", subject: "Feedback from #{@user.email}"
+    mail to: ENV['MAIN_ADMIN'], subject: "Feedback from #{@user.email}"
   end
 end
