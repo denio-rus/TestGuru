7e0a09f0d237f28dc01ad0363709569170e0c937
denio-rus <denio@yandex.ru>
2018-11-08 21:52:25 +0700

    badge. string-condition

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,2 +1,48 @@
-class Admin::BadgesController < ApplicationController
+class Admin::BadgesController < Admin::BaseController
+  before_action :set_badge, only: %i[edit destroy update show]
+
+  def index
+    @badges = Badge.all
+  end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def create
+    @badge = Badge.new(badge_params)
+
+    if @badge.save
+      redirect_to admin_badges_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badge_path(@badge);
+    else
+      render :edit
+    end
+  end
+
+  def show; end
+
+  private
+
+  def set_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:title, :description, :condition)
+  end
 end
--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,4 +2,7 @@ class Badge < ApplicationRecord
   has_many :achievements
   has_many :users, through: :achievements
   has_many :test_passages, through: :achievements
+
+  validates :title, :condition, :description, presence: true
+  validates :title, :condition, uniqueness: true
 end
