aca76ed1eb5dbb9ccb886d2b7b71bea0b29963e2
denio-rus <denio@yandex.ru>
2018-11-10 01:06:59 +0700

    lesson18. badges.first try

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -45,20 +45,4 @@ class Admin::BadgesController < Admin::BaseController
   def badge_params
     params.require(:badge).permit(:title, :image, :description, :choice, :subject)
   end
-
-  def make_the_rule(choice, subject)
-    case choice
-    when "1"
-      "Category.find_by(title: #{subject}).tests.ids.sort == test_passage.category.test_passages.all_successful.where(user: current_user).pluck(:test_id).uniq.sort"
-    when "2"
-      "level = test_passage.test.level
-      list = []
-      current_user.test_passages.all_successful.each { |test_passage| list << test_passage.test.id if test_passage.test.level == level}
-      Test.where(level: #{subject}.to_i).ids.sort == list.uniq.sort"
-    when "3"
-      "test_passage.test.id == Test.find_by(title: #{subject}).id && test_passage.id == current_user.test_passages.where(test: test_passage.test).ids.sort.first"
-    when "4"
-      "current_user.test_passages.all_successful.count == #{subject}.to_i"
-    end
-  end
 end
--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,5 +1,6 @@
 class Badge < ApplicationRecord
   attr_accessor :choice, :subject
+
   has_many :achievements, dependent: :destroy
   has_many :users, through: :achievements
   has_many :test_passages, through: :achievements
@@ -10,16 +11,16 @@ class Badge < ApplicationRecord
   before_validation :make_the_rule
 
   def make_the_rule
-    case choice
+    case choice.to_i
     when 1
-      self.condition = "Category.find_by(title: #{subject}).tests.ids.sort == test_passage.category.test_passages.all_successful.where(user: current_user).pluck(:test_id).uniq.sort"
+      self.condition = "Category.find_by(title: '#{subject}').tests.ids.sort == test_passage.category.test_passages.all_successful.where(user: current_user).pluck(:test_id).uniq.sort"
     when 2
       self.condition = "level = test_passage.test.level
       list = []
       current_user.test_passages.all_successful.each { |test_passage| list << test_passage.test.id if test_passage.test.level == level}
       Test.where(level: #{subject}.to_i).ids.sort == list.uniq.sort"
     when 3
-      self.condition = "test_passage.test.id == Test.find_by(title: #{subject}).id && test_passage.id == current_user.test_passages.where(test: test_passage.test).ids.sort.first"
+      self.condition = "test_passage.test.id == Test.find_by(title: '#{subject}').id && test_passage.id == current_user.test_passages.where(test: test_passage.test).ids.sort.first"
     when 4
       self.condition = "current_user.test_passages.all_successful.count == #{subject}.to_i"
     end
